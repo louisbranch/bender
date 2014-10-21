41a1c9d5dd9aef420825c8338140eafe9dbf8e53
Luiz Branco <me@luizbranco.com>
2014-10-21 03:59:35 -0400

    Initial commit
    Add grid of random colored tiles to scene

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public GameObject tile;
+	public static int width = 8;
+	public static int height = 6;
+	private GameObject [,] grid = new GameObject[width,height];
+
+	void Awake () {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				GameObject t = (GameObject)Instantiate(tile);
+				t.transform.position = new Vector3(-width/2 + x, -height/2 + y, t.transform.position.z);
+				t.renderer.material.color = new Color(Random.value,Random.value,Random.value,1);
+				grid[x,y] = t;
+			}	
+		}
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
